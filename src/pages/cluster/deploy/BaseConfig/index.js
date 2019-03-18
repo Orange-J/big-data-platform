f7a142b0bd0d8e671c8d1cfba182d125ab279fea
OrangeJ <juhaotian@foxmail.com>
2019-03-19 00:07:37 +0800

    [refactor] 部署流程-baseConfig无需再使用AntDesgin的Form装饰

--- src/pages/cluster/deploy/BaseConfig/index.js
+++ src/pages/cluster/deploy/BaseConfig/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, message } from 'antd';
+import { message } from 'antd';
 import ServerTable from './ServerTable';
 import styles from './index.module.scss';
 
@@ -36,27 +36,12 @@ class BaseConfig extends Component {
     }
 
     render () {
-        const serverTableItemLayout = {
-            labelCol: {
-                span: 3
-            },
-            wrapperCol: {
-                span: 21
-            }
-        };
-
         return (
             <div className={styles.BaseConfigWrap}>
-                <Form>
-                    <Form.Item label="服务器节点配置" {...serverTableItemLayout}>
-                        <ServerTable />
-                    </Form.Item>
-                </Form>
+                <ServerTable />
             </div>
         );
     }
 }
-
-BaseConfig = Form.create({name: 'base_config_form'})(BaseConfig);
 
 export default BaseConfig;
